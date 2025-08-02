2334bd0f1e075e136dcf0fa51a3f71d86313eb6b
KSBM009 <kevin@emp.ogesinfotech.com>
2025-08-02 14:58:25 +0530

    Cleaning the git repository is completed.

---
+++ LaundryApp/cartSectionTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  cartSectionTableViewCell.swift
+//  LaundryApp
+//
+//  Created by macbook on 22/05/2025.
+//
+
+import UIKit
+
+class cartSectionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var Category: UILabel!
+    @IBOutlet weak var count: UILabel!
+    @IBOutlet weak var showBtn: UIButton!
+    
+    static let identifier = "cartSectionTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "cartSectionTableViewCell", bundle: nil)
+    }
+    
+}
